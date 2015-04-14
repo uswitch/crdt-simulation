87497eba9bf9387afa686c6791b9fc821cdfbec1
Thomas Kristensen <thomas.kristensen@uswitch.com>
2015-04-14 21:34:27 +0100

    Initial working commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject g-counter "0.1.0-SNAPSHOT"
+  :description "G counter simulation"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/tools.logging "0.3.1"]])
